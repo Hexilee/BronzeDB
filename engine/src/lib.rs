5bddb0df0c66192482be72b64d85dfe92e75e6d4
Hexilee <hexileee@gmail.com>
2019-04-08 01:21:55 +0800

    complete Key

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,15 +1,15 @@
 #[macro_use(matches)]
 extern crate matches;
 
-use crate::util::{Entry, Key, Value};
+use crate::util::{Entry, RawKey, Value};
 
 pub trait Engine {
-    fn set(key: &Key, value: Value) -> status::Result<()>;
-    fn get(key: &Key, value: &mut Value) -> status::Result<()>;
-    fn delete(key: &Key) -> status::Result<()>;
+    fn set(key: &RawKey, value: Value) -> status::Result<()>;
+    fn get(key: &RawKey, value: &mut Value) -> status::Result<()>;
+    fn delete(key: &RawKey) -> status::Result<()>;
     fn scan(
-        lower_bound: Option<&Key>,
-        upper_bound: Option<&Key>,
+        lower_bound: Option<&RawKey>,
+        upper_bound: Option<&RawKey>,
         visitor: impl Fn(&Entry) -> status::Result<()>,
     ) -> status::Result<usize>;
 }
--- engine/src/util.rs
+++ engine/src/util.rs
@@ -5,26 +5,37 @@ use std::ops::Deref;
 use std::os::raw::c_void;
 
 pub type Value = Vec<u8>;
-pub type Entry = (Key, Value);
+pub type Entry = (RawKey, Value);
 
 pub struct Key {
+    data: Vec<u8>,
+}
+
+pub struct RawKey {
     data: [u8],
 }
 
-impl Deref for Key {
+impl Deref for RawKey {
     type Target = [u8];
     fn deref(&self) -> &Self::Target {
         &self.data
     }
 }
 
-impl From<&[u8]> for &Key {
+impl Deref for Key {
+    type Target = Vec<u8>;
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl From<&[u8]> for &RawKey {
     fn from(data: &[u8]) -> Self {
         unsafe { transmute(data) }
     }
 }
 
-impl PartialEq for Key {
+impl PartialEq for RawKey {
     fn eq(&self, other: &Self) -> bool {
         self.len() == other.len()
             && unsafe {
@@ -37,7 +48,7 @@ impl PartialEq for Key {
     }
 }
 
-impl PartialOrd for Key {
+impl PartialOrd for RawKey {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match unsafe {
             memcmp(
@@ -61,22 +72,34 @@ impl PartialOrd for Key {
     }
 }
 
+impl PartialEq for Key {
+    fn eq(&self, other: &Self) -> bool {
+        <RawKey as PartialEq>::eq(self.as_slice().into(), other.as_slice().into())
+    }
+}
+
+impl PartialOrd for Key {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        <RawKey as PartialOrd>::partial_cmp(self.as_slice().into(), other.as_slice().into())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Key;
+    use super::RawKey;
     use std::cmp::Ordering::*;
     use std::cmp::PartialOrd;
     #[test]
     fn key_cmp() {
         for (former, latter, order) in vec![
-            (&b"xixi"[..], &b"haha"[..], Greater),
-            (&b"haa"[..], &b"haha"[..], Less),
-            (&b"haha"[..], &b"haha"[..], Equal),
-            (&b"hah"[..], &b"haha"[..], Less),
-            (&b"hahah"[..], &b"haha"[..], Greater),
+            ("xixi", "haha", Greater),
+            ("haa", "haha", Less),
+            ("haha", "haha", Equal),
+            ("hah", "haha", Less),
+            ("hahah", "haha", Greater),
         ] {
-            let former_key: &Key = former.into();
-            let latter_key: &Key = latter.into();
+            let former_key = former.to_owned().into_bytes();
+            let latter_key = latter.to_owned().into_bytes();
             assert_eq!(order, former_key.partial_cmp(&latter_key).unwrap());
         }
     }
