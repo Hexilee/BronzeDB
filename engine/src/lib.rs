43b6b3c0040b21a13db7d5aa513fffe29b16da58
Hexilee <hexileee@gmail.com>
2019-04-08 00:33:19 +0800

    test::key_cmp pass

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,14 +1,17 @@
+#[macro_use(matches)]
+extern crate matches;
+
 use crate::util::{Entry, Key, Value};
 
 pub trait Engine {
-    fn set(entities: impl Iterator<Item = Entry>) -> status::Result<usize>;
-    fn get(key: &Key) -> status::Result<Value>;
-    fn delete(keys: impl Iterator<Item = Key>) -> status::Result<usize>;
-    fn scan(
-        lower_bound: Option<&Key>,
-        upper_bound: Option<&Key>,
-        visitor: impl Fn(&Entry) -> status::Result<()>,
-    ) -> status::Result<usize>;
+    //    fn set(entities: impl Iterator<Item = Entry>) -> status::Result<usize>;
+    //    fn get(key: &Key) -> status::Result<Value>;
+    //    fn delete(keys: impl Iterator<Item = Key>) -> status::Result<usize>;
+    //    fn scan(
+    //        lower_bound: Option<&Key>,
+    //        upper_bound: Option<&Key>,
+    //        visitor: impl Fn(&Entry) -> status::Result<()>,
+    //    ) -> status::Result<usize>;
 }
 
 pub mod status;
--- engine/src/util.rs
+++ engine/src/util.rs
@@ -1,6 +1,82 @@
-use num_bigint::BigUint;
+use libc::memcmp;
+use std::cmp::{Ordering, PartialEq, PartialOrd};
+use std::ops::Deref;
+use std::os::raw::c_void;
 
-pub type Key = BigUint;
-pub type Value = Vec<u8>;
+pub type Value<'a> = &'a [u8];
+pub type Entry<'a> = (Key<'a>, Value<'a>);
 
-pub type Entry = (Key, Value);
+pub struct Key<'a> {
+    data: &'a [u8],
+}
+
+impl<'a> Deref for Key<'a> {
+    type Target = [u8];
+    fn deref(&self) -> &Self::Target {
+        self.data
+    }
+}
+
+impl<'a> From<&'a [u8]> for Key<'a> {
+    fn from(data: &'a [u8]) -> Self {
+        Self { data }
+    }
+}
+
+impl<'a> PartialEq for Key<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.len() == other.len()
+            && unsafe {
+                0 == memcmp(
+                    &self.data[0] as *const u8 as *const c_void,
+                    &other.data[0] as *const u8 as *const c_void,
+                    self.len(),
+                )
+            }
+    }
+}
+
+impl<'a> PartialOrd for Key<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match unsafe {
+            memcmp(
+                &self.data[0] as *const u8 as *const c_void,
+                &other.data[0] as *const u8 as *const c_void,
+                self.len(),
+            )
+        } {
+            x if x < 0 => Some(Ordering::Less),
+            x if x > 0 => Some(Ordering::Greater),
+            _ => {
+                if self.len() == other.len() {
+                    Some(Ordering::Equal)
+                } else if self.len() < other.len() {
+                    Some(Ordering::Less)
+                } else {
+                    Some(Ordering::Greater)
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Key;
+    use std::cmp::Ordering::*;
+    use std::cmp::PartialOrd;
+    #[test]
+    fn key_cmp() {
+        for (former, latter, order) in vec![
+            (&b"xixi"[..], &b"haha"[..], Greater),
+            (&b"haa"[..], &b"haha"[..], Less),
+            (&b"haha"[..], &b"haha"[..], Equal),
+            (&b"hah"[..], &b"haha"[..], Less),
+            (&b"hahah"[..], &b"haha"[..], Greater),
+        ] {
+            let former_key: Key = former.into();
+            let latter_key = latter.into();
+            assert_eq!(order, former_key.partial_cmp(&latter_key).unwrap());
+        }
+    }
+}
