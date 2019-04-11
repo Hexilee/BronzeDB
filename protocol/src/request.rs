1962f813ae94eecf4a7fe937542a3b31e0b90df0
Hexilee <hexileee@gmail.com>
2019-04-12 06:45:36 +0800

    complete all protocol::request tests

--- protocol/src/request.rs
+++ protocol/src/request.rs
@@ -70,7 +70,7 @@ impl Request {
                 };
                 let upper_key = match upper_bound.as_ref() {
                     Some(key) => key.deref(),
-                    None => MIN_KEY,
+                    None => MAX_KEY,
                 };
                 counter += writer.write_key(&lower_key)?;
                 counter += writer.write_key(&upper_key)?;
@@ -169,35 +169,183 @@ mod tests {
         }
     }
 
-    #[test]
-    fn request_get_test() {
-        let (new_request, bytes) = Request::Get(b"name"[..].to_vec().into())
-            .transfer_move()
-            .unwrap();
-        assert_eq!(7usize, bytes);
-        assert!(matches!(&new_request, Request::Get(ref _key)));
-        if let Request::Get(ref key) = new_request {
-            assert_eq!(&b"name"[..], key.as_slice());
+    macro_rules! assert_get {
+        ($data:expr) => {
+            let (new_request, bytes) = Request::Get($data[..].to_vec().into())
+                .transfer_move()
+                .unwrap();
+            assert_eq!($data[..].len() + 3, bytes);
+            assert!(matches!(&new_request, Request::Get(ref _key)));
+            if let Request::Get(ref key) = new_request {
+                assert_eq!(&$data[..], key.as_slice());
+            }
+        };
+    }
+
+    speculate! {
+        describe "get" {
+            it "normal" {
+                assert_get!(b"name");
+            }
+
+            it "zero" {
+                assert_get!([0; 0]);
+            }
+
+            it "max length" {
+                assert_get!([0; MAX_KEY_LEN]);
+            }
+
+            #[should_panic]
+            it "overflow" {
+                assert_get!([0; MAX_KEY_LEN + 1]);
+            }
         }
     }
 
-    #[test]
-    fn request_scan_test() {
-        let (new_request, bytes) = Request::Scan {
-            lower_bound: None,
-            upper_bound: Some(b"name"[..].to_vec().into()),
+    macro_rules! assert_scan {
+        () => {
+            let (new_request, bytes) = Request::Scan {
+                lower_bound: None,
+                upper_bound: None,
+            }
+            .transfer_move()
+            .unwrap();
+            assert_eq!(1 + 4 + MAX_KEY_LEN * 2, bytes);
+            assert!(matches!(&new_request, Request::Scan{lower_bound: _, upper_bound: _}));
+            if let Request::Scan {
+                lower_bound,
+                upper_bound,
+            } = new_request
+            {
+                assert!(matches!(lower_bound, None));
+                assert!(matches!(upper_bound, None));
+            }
+        };
+
+        ($lower_bound:expr, $upper_bound:expr) => {
+            let (new_request, bytes) = Request::Scan {
+                lower_bound: Some($lower_bound[..].to_vec().into()),
+                upper_bound: Some($upper_bound[..].to_vec().into()),
+            }
+            .transfer_move()
+            .unwrap();
+            assert_eq!(1 + 4 + $lower_bound[..].len() + $upper_bound[..].len(), bytes);
+            assert!(matches!(&new_request, Request::Scan{lower_bound: _, upper_bound: _}));
+            if let Request::Scan {
+                lower_bound,
+                upper_bound,
+            } = new_request
+            {
+                assert!(matches!(lower_bound, Some(ref _key)));
+                assert!(matches!(upper_bound, Some(ref _key)));
+                assert_eq!(&$lower_bound[..], lower_bound.unwrap().as_slice());
+                assert_eq!(&$upper_bound[..], upper_bound.unwrap().as_slice());
+            }
+        };
+
+        ($any_bound:expr) => {
+            { // lower_bound
+                let (new_request, bytes) = Request::Scan {
+                    lower_bound: Some($any_bound[..].to_vec().into()),
+                    upper_bound: None,
+                }
+                .transfer_move()
+                .unwrap();
+                assert_eq!(1 + 4 + $any_bound[..].len() + MAX_KEY_LEN, bytes);
+                assert!(matches!(&new_request, Request::Scan{lower_bound: _, upper_bound: _}));
+                if let Request::Scan {
+                    lower_bound,
+                    upper_bound,
+                } = new_request
+                {
+                    assert!(matches!(lower_bound, Some(ref _key)));
+                    assert!(matches!(upper_bound, None));
+                    assert_eq!(&$any_bound[..], lower_bound.unwrap().as_slice());
+                }
+            }
+            { // upper_bound
+                let (new_request, bytes) = Request::Scan {
+                    lower_bound: None,
+                    upper_bound: Some($any_bound[..].to_vec().into()),
+                }
+                .transfer_move()
+                .unwrap();
+                assert_eq!(1 + 4 + MAX_KEY_LEN + $any_bound[..].len(), bytes);
+                assert!(matches!(&new_request, Request::Scan{lower_bound: _, upper_bound: _}));
+                if let Request::Scan {
+                    lower_bound,
+                    upper_bound,
+                } = new_request
+                {
+                    assert!(matches!(lower_bound, None));
+                    assert!(matches!(upper_bound, Some(ref _key)));
+                    assert_eq!(&$any_bound[..], upper_bound.unwrap().as_slice());
+                }
+            }
+        };
+    }
+
+    speculate! {
+        use std::u8::MAX;
+
+        describe "scan with two bounds" {
+            it "normal" {
+                assert_scan!(b"last_name", b"name");
+            }
+
+            it "zero" {
+                assert_scan!([0; 0], [0; 0]);
+            }
+
+            it "max length" {
+                assert_scan!([1; MAX_KEY_LEN], [MAX - 1; MAX_KEY_LEN]);
+            }
+
+            #[should_panic] // should be (None, None)
+            it "max range" {
+                assert_scan!([0; MAX_KEY_LEN], [MAX; MAX_KEY_LEN]);
+            }
+
+            #[should_panic]
+            it "overflow" {
+                assert_scan!([0; MAX_KEY_LEN + 1], [MAX; MAX_KEY_LEN + 1]);
+            }
         }
-        .transfer_move()
-        .unwrap();
-        assert_eq!(265usize, bytes);
-        assert!(matches!(&new_request, Request::Scan{lower_bound: _, upper_bound: _}));
-        if let Request::Scan {
-            lower_bound,
-            upper_bound,
-        } = new_request
-        {
-            assert!(matches!(lower_bound, None));
-            assert!(matches!(upper_bound, Some(_key)));
+
+        describe "scan with one bound" {
+            it "normal" {
+                assert_scan!(b"last_name");
+            }
+
+            it "zero" {
+                assert_scan!([0; 0]);
+            }
+
+            it "max length" {
+                assert_scan!([1; MAX_KEY_LEN]);
+            }
+
+            #[should_panic] // should be (None, None)
+            it "min lower_bound" {
+                assert_scan!([0; MAX_KEY_LEN]);
+            }
+
+            #[should_panic] // should be (None, None)
+            it "max upper_bound" {
+                assert_scan!([MAX; MAX_KEY_LEN]);
+            }
+
+            #[should_panic]
+            it "overflow" {
+                assert_scan!([0; MAX_KEY_LEN + 1]);
+            }
+        }
+
+        describe "scan with no bound" {
+            it "normal" {
+                assert_scan!();
+            }
         }
     }
 
@@ -212,6 +360,47 @@ mod tests {
         if let Request::Set(ref key, ref value) = new_request {
             assert_eq!(&b"last_name"[..], key.as_slice());
             assert_eq!(&b"Lee"[..], value.as_slice());
+        }
+    }
+
+    macro_rules! assert_set {
+        ($key:expr, $value:expr) => {
+            let (new_request, bytes) =
+                Request::Set($key[..].to_vec().into(), $value[..].to_vec().into())
+                    .transfer_move()
+                    .unwrap();
+            assert_eq!(5 + $key.len() + $value.len(), bytes);
+            assert!(matches!(&new_request, Request::Set(ref _key, ref _value)));
+            if let Request::Set(ref key, ref value) = new_request {
+                assert_eq!(&$key[..], key.as_slice());
+                assert_eq!(&$value[..], value.as_slice());
+            }
+        };
+    }
+
+    speculate! {
+        describe "set" {
+            it "normal" {
+                assert_set!(b"name", b"hexi");
+            }
+
+            it "zero" {
+                assert_set!([0; 0], [0; 0]);
+            }
+
+            it "max length" {
+                assert_set!([0; MAX_KEY_LEN], [0; MAX_VALUE_LEN]);
+            }
+
+            #[should_panic]
+            it "key overflow" {
+                assert_set!([0; MAX_KEY_LEN + 1], [0; MAX_VALUE_LEN]);
+            }
+
+            #[should_panic]
+            it "value overflow" {
+                assert_set!([0; MAX_KEY_LEN], [0; MAX_VALUE_LEN + 1]);
+            }
         }
     }
 }
