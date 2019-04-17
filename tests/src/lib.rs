5d3e83a6c41f306385bb5bb0da912943bcb7edc4
Hexilee <hexileee@gmail.com>
2019-04-17 23:37:03 +0800

    complete one_connect test

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -2,6 +2,9 @@
 
 #[macro_use]
 extern crate serde_derive;
+use client::Client;
+use std::net::TcpStream;
+use std::time::Instant;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
@@ -16,22 +19,48 @@ impl Config {
     }
 }
 
-mod set {
-    use super::Config;
-    use client::Client;
-    use std::net::TcpStream;
-    use std::time::Instant;
+fn get_client() -> Client<TcpStream> {
+    let conn = TcpStream::connect(&Config::new().db_addr).unwrap();
+    Client::new(conn)
+}
 
-    #[test]
-    fn one_connect() {
-        let conn = TcpStream::connect(&Config::new().db_addr).unwrap();
-        let mut client = Client::new(conn);
+#[test]
+fn one_connect() {
+    let mut client = get_client();
+    const SIZE: u64 = 100_000;
+    {
         let now = Instant::now();
-        for i in 0..100_000 {
+        for i in 0..SIZE {
             let value = i.to_string().into_bytes();
             let key = value.clone().into();
             client.set(key, value).unwrap();
         }
-        println!("one connect set: {}/s", 100_000 / now.elapsed().as_secs());
+        println!("one connect set: {}/s", SIZE / now.elapsed().as_secs());
+    }
+    {
+        let now = Instant::now();
+        for i in 0..SIZE {
+            let value = i.to_string().into_bytes();
+            let key = value.clone().into();
+            debug_assert_eq!(value, client.get(key).unwrap().unwrap());
+        }
+        println!("one connect get: {}/s", SIZE / now.elapsed().as_secs());
+    }
+    {
+        let now = Instant::now();
+        let (size, scanner) = client.scan(None, None).unwrap();
+        for item in scanner {
+            let (key, value) = item.unwrap();
+            debug_assert_eq!(value.as_slice(), key.as_slice());
+        }
+        println!("one connect scan: {}/s", size as u64 / now.elapsed().as_secs());
+    }
+    {
+        let now = Instant::now();
+        for i in 0..SIZE {
+            let key = i.to_string().into_bytes().into();
+            client.delete(key).unwrap();
+        }
+        println!("one connect delete: {}/s", SIZE / now.elapsed().as_secs());
     }
 }
