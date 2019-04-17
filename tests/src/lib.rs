60ee6fc0a684fe365c56412d3d7545b695170129
Hexilee <hexileee@gmail.com>
2019-04-18 00:01:19 +0800

    connection reset in threads

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -2,6 +2,7 @@
 
 #[macro_use]
 extern crate serde_derive;
+
 use client::Client;
 use std::net::TcpStream;
 use std::time::Instant;
@@ -53,7 +54,10 @@ fn one_connect() {
             let (key, value) = item.unwrap();
             debug_assert_eq!(value.as_slice(), key.as_slice());
         }
-        println!("one connect scan: {}/s", size as u64 / now.elapsed().as_secs());
+        println!(
+            "one connect scan: {}/s",
+            size as u64 / now.elapsed().as_secs()
+        );
     }
     {
         let now = Instant::now();
@@ -62,5 +66,32 @@ fn one_connect() {
             client.delete(key).unwrap();
         }
         println!("one connect delete: {}/s", SIZE / now.elapsed().as_secs());
+    }
+}
+
+#[test]
+fn multi_connect() {
+    const THREADS: u64 = 1000;
+    const SIZE: u64 = 100;
+    {
+        let now = Instant::now();
+        let mut handlers = Vec::with_capacity(THREADS as usize);
+        for id in 0..THREADS {
+            handlers.push(std::thread::spawn(move || {
+                let mut client = get_client();
+                for i in id * SIZE..(id + 1) * SIZE {
+                    let value = i.to_string().into_bytes();
+                    let key = value.clone().into();
+                    client.set(key, value).unwrap();
+                }
+            }));
+        }
+        for handler in handlers {
+            handler.join().unwrap();
+        }
+        println!(
+            "multi connect set: {}/s",
+            THREADS * SIZE / now.elapsed().as_secs()
+        );
     }
 }
