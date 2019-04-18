78bb5fac84fa3f5f60eceffc53ff9dbc67d7ca88
Hexilee <hexileee@gmail.com>
2019-04-18 17:05:01 +0800

    complete multi connect test

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg(test)]
+#![feature(duration_float)]
 
 #[macro_use]
 extern crate serde_derive;
@@ -37,7 +38,10 @@ fn one_connect() -> Result<()> {
             let key = value.clone().into();
             client.set(key, value)?;
         }
-        println!("one connect set: {}/s", SIZE / now.elapsed().as_secs());
+        println!(
+            "one connect set: {}/s",
+            SIZE as f64 / now.elapsed().as_secs_f64()
+        );
     }
     {
         let now = Instant::now();
@@ -46,7 +50,10 @@ fn one_connect() -> Result<()> {
             let key = value.clone().into();
             debug_assert_eq!(value, client.get(key)?.unwrap());
         }
-        println!("one connect get: {}/s", SIZE / now.elapsed().as_secs());
+        println!(
+            "one connect get: {}/s",
+            SIZE as f64 / now.elapsed().as_secs_f64()
+        );
     }
     {
         let now = Instant::now();
@@ -57,7 +64,7 @@ fn one_connect() -> Result<()> {
         }
         println!(
             "one connect scan: {}/s",
-            size as u64 / now.elapsed().as_secs()
+            size as f64 / now.elapsed().as_secs_f64()
         );
     }
     {
@@ -66,7 +73,10 @@ fn one_connect() -> Result<()> {
             let key = i.to_string().into_bytes().into();
             client.delete(key)?;
         }
-        println!("one connect delete: {}/s", SIZE / now.elapsed().as_secs());
+        println!(
+            "one connect delete: {}/s",
+            SIZE as f64 / now.elapsed().as_secs_f64()
+        );
     }
     Ok(())
 }
@@ -99,8 +109,78 @@ fn multi_connect() -> Result<()> {
         }
         println!(
             "multi connect set: {}/s",
-            THREADS * SIZE / now.elapsed().as_secs()
+            (THREADS * SIZE) as f64 / now.elapsed().as_secs_f64()
         );
-        Ok(())
     }
+    {
+        let now = Instant::now();
+        let mut handlers = Vec::with_capacity(THREADS as usize);
+        for id in 0..THREADS {
+            let pool = pool.clone();
+            handlers.push(std::thread::spawn(move || -> Result<()> {
+                for i in id * SIZE..(id + 1) * SIZE {
+                    let value = i.to_string().into_bytes();
+                    let key = value.clone().into();
+                    debug_assert_eq!(value, pool.get().unwrap().get(key)?.unwrap());
+                }
+                Ok(())
+            }));
+        }
+        for handler in handlers {
+            handler.join().unwrap()?;
+        }
+        println!(
+            "multi connect get: {}/s",
+            (THREADS * SIZE) as f64 / now.elapsed().as_secs_f64()
+        );
+    }
+    {
+        let now = Instant::now();
+        let mut handlers = Vec::with_capacity(THREADS as usize);
+        for id in 0..THREADS {
+            let pool = pool.clone();
+            handlers.push(std::thread::spawn(move || -> Result<usize> {
+                let lower_key = (id * SIZE).to_string().into_bytes().into();
+                let upper_key = ((id + 1) * SIZE - 1).to_string().into_bytes().into();
+                let mut client = pool.get().unwrap();
+                let (size, scanner) = client.scan(Some(lower_key), Some(upper_key))?;
+                for item in scanner {
+                    let (key, value) = item?;
+                    debug_assert_eq!(value.as_slice(), key.as_slice());
+                }
+                Ok(size)
+            }));
+        }
+
+        let mut counter = 0;
+        for handler in handlers {
+            counter += handler.join().unwrap()?;
+        }
+        println!(
+            "multi connect scan: {}/s",
+            counter as f64 / now.elapsed().as_secs_f64()
+        );
+    }
+    {
+        let now = Instant::now();
+        let mut handlers = Vec::with_capacity(THREADS as usize);
+        for id in 0..THREADS {
+            let pool = pool.clone();
+            handlers.push(std::thread::spawn(move || -> Result<()> {
+                for i in id * SIZE..(id + 1) * SIZE {
+                    let key = i.to_string().into_bytes().into();
+                    pool.get().unwrap().delete(key)?;
+                }
+                Ok(())
+            }));
+        }
+        for handler in handlers {
+            handler.join().unwrap()?;
+        }
+        println!(
+            "multi connect delete: {}/s",
+            (THREADS * SIZE) as f64 / now.elapsed().as_secs_f64()
+        );
+    }
+    Ok(())
 }
