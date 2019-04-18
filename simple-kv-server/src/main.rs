27d129b5d2702733d6176d5909ff5e19b74c11dc
Hexilee <hexileee@gmail.com>
2019-04-18 14:11:42 +0800

    add logs

--- simple-kv-server/src/main.rs
+++ simple-kv-server/src/main.rs
@@ -2,6 +2,7 @@ use crate::engine_impl::EngineImpl;
 use engine::Engine;
 use protocol::request::Request::{self, *};
 use protocol::response::Response;
+use std::io::ErrorKind;
 use std::net::{TcpListener, TcpStream};
 use std::thread::spawn;
 use util::status::StatusCode::*;
@@ -9,51 +10,56 @@ use util::status::{Error, Result};
 
 fn handle_client<T: Engine>(mut stream: TcpStream, mut engine: T) -> Result<()> {
     loop {
-        match Request::read_from(&mut stream)? {
-            Get(key) => match engine.get(key.into()) {
-                Ok(value) => {
-                    Response::SingleValue { status: OK, value }.write_to(&mut stream)?;
-                }
-                Err(err) => {
-                    Response::Status(err.code).write_to(&mut stream)?;
-                }
-            },
-            Set(key, value) => match engine.set(key.into(), value) {
-                Ok(_) => {
-                    Response::Status(OK).write_to(&mut stream)?;
-                }
-                Err(err) => {
-                    Response::Status(err.code).write_to(&mut stream)?;
-                }
-            },
-            Delete(key) => match engine.delete(key.into()) {
-                Ok(_) => {
-                    Response::Status(OK).write_to(&mut stream)?;
-                }
-                Err(err) => {
-                    Response::Status(err.code).write_to(&mut stream)?;
-                }
-            },
-            Scan {
-                lower_bound,
-                upper_bound,
-            } => match engine.scan(lower_bound, upper_bound) {
-                Ok(scanner) => {
-                    Response::MultiKV {
-                        status: OK,
-                        size: scanner.size(),
-                        iter: scanner.iter(),
+        match Request::read_from(&mut stream) {
+            Ok(request) => match request {
+                Get(key) => match engine.get(key.into()) {
+                    Ok(value) => {
+                        Response::SingleValue { status: OK, value }.write_to(&mut stream)?;
                     }
-                    .write_to(&mut stream)?;
-                }
-                Err(err) => {
-                    Response::Status(err.code).write_to(&mut stream)?;
+                    Err(err) => {
+                        Response::Status(err.code).write_to(&mut stream)?;
+                    }
+                },
+                Set(key, value) => match engine.set(key.into(), value) {
+                    Ok(_) => {
+                        Response::Status(OK).write_to(&mut stream)?;
+                    }
+                    Err(err) => {
+                        Response::Status(err.code).write_to(&mut stream)?;
+                    }
+                },
+                Delete(key) => match engine.delete(key.into()) {
+                    Ok(_) => {
+                        Response::Status(OK).write_to(&mut stream)?;
+                    }
+                    Err(err) => {
+                        Response::Status(err.code).write_to(&mut stream)?;
+                    }
+                },
+                Scan {
+                    lower_bound,
+                    upper_bound,
+                } => match engine.scan(lower_bound, upper_bound) {
+                    Ok(scanner) => {
+                        Response::MultiKV {
+                            status: OK,
+                            size: scanner.size(),
+                            iter: scanner.iter(),
+                        }
+                        .write_to(&mut stream)?;
+                    }
+                    Err(err) => {
+                        Response::Status(err.code).write_to(&mut stream)?;
+                    }
+                },
+                Unknown => {
+                    Response::Status(UnknownAction).write_to(&mut stream)?;
+                    break Err(Error::new(UnknownAction, "unknown action"));
                 }
             },
-            Unknown => {
-                Response::Status(UnknownAction).write_to(&mut stream)?;
-                break Err(Error::new(UnknownAction, "unknown action"))
-            },
+
+            Err(ref err) if err.kind() == ErrorKind::UnexpectedEof => break Ok(()), // shutdown
+            Err(err) => break Err(err.into()),
         }
     }
 }
@@ -64,7 +70,11 @@ fn main() -> Result<()> {
     for stream in listener.incoming() {
         let engine = shared_engine.clone();
         let stream = stream?;
-        spawn(move || match handle_client(stream, engine) {
+        println!("establish connection from {}", stream.peer_addr()?);
+        spawn(move || {
+            let addr = stream.peer_addr().unwrap();
+            handle_client(stream, engine).unwrap();
+            println!("close connection from {}", addr);
         });
     }
     Ok(())
