ce15def2c8c6a0b765bf44cc7ed9249723f672d9
Hexilee <hexileee@gmail.com>
2019-04-19 21:53:39 +0800

    ready to remove size in scan

--- sled-db-server/src/engine_impl.rs
+++ sled-db-server/src/engine_impl.rs
@@ -1,7 +1,31 @@
 use engine::{Engine, Scanner};
 use sled::Db;
-use util::status::Result;
+use std::path;
+use util::status::{Error, StatusCode};
 use util::types::Key;
+
+#[derive(Debug)]
+pub struct EngineError {
+    inner: sled::Error,
+}
+
+impl EngineError {
+    pub fn new(err: sled::Error) -> Self {
+        Self { inner: err }
+    }
+}
+
+impl From<sled::Error> for EngineError {
+    fn from(err: sled::Error) -> Self {
+        Self::new(err)
+    }
+}
+
+impl From<EngineError> for Error {
+    fn from(err: EngineError) -> Self {
+        Error::new(StatusCode::EngineError, err.inner.to_string())
+    }
+}
 
 #[derive(Clone)]
 pub struct EngineImpl {
@@ -9,7 +33,7 @@ pub struct EngineImpl {
 }
 
 impl EngineImpl {
-    pub fn new(path: impl AsRef<std::path::Path>) -> Self {
+    pub fn new(path: impl AsRef<path::Path>) -> Self {
         Self {
             inner: Db::start_default(path.as_ref())
                 .expect(&format!("cannot open db {:?}", path.as_ref())),
@@ -18,21 +42,32 @@ impl EngineImpl {
 }
 
 impl Engine for EngineImpl {
-    type Error = ();
+    type Error = EngineError;
 
     fn set(&mut self, key: Key, value: Vec<u8>) -> Result<(), Self::Error> {
-        unimplemented!()
+        self.inner.set(key, value)?;
+        Ok(())
+
     }
 
     fn get(&self, key: Key) -> Result<Option<Vec<u8>>, Self::Error> {
-        unimplemented!()
+        Ok(self.inner.get(key)?.map(|data| data.to_vec()))
     }
 
     fn delete(&mut self, key: Key) -> Result<(), Self::Error> {
-        unimplemented!()
+        self.inner.del(key)?;
+        Ok(())
     }
 
-    fn scan(&self, lower_bound: Option<Key>, upper_bound: Option<Key>) -> Result<Box<Scanner>, Self::Error> {
-        unimplemented!()
+    fn scan(
+        &self,
+        lower_bound: Option<Key>,
+        upper_bound: Option<Key>,
+    ) -> Result<Box<Scanner>, Self::Error> {
+        self.inner.scan()
     }
+}
+
+pub struct SledScanner {
+    iter
 }
