9753f85bf16ff87e8c58d09328b6b9c801f389dc
Hexilee <hexileee@gmail.com>
2019-05-30 12:19:36 +0800

    add config and README for memory-db-server

--- bronzedb-memory-db-server/src/main.rs
+++ bronzedb-memory-db-server/src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate serde_derive;
 use crate::engine_impl::EngineImpl;
 use bronzedb_server::Server;
 use bronzedb_util::status::Result;
@@ -5,8 +7,10 @@ use std::net::TcpListener;
 
 fn main() -> Result<()> {
     env_logger::init();
-    let listener = TcpListener::bind("127.0.0.1:8088")?;
+    let config = conf::Config::new();
+    let listener = TcpListener::bind(&config.db_addr)?;
     Server::new(EngineImpl::new()).serve(listener)
 }
 
 mod engine_impl;
+mod conf;
